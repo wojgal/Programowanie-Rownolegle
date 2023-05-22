a189b0b845e182dec77884b5e6d53ef51b1ec85e
Wojciech Galewski <81717259+wojgal@users.noreply.github.com>
2023-05-22 23:47:59 +0200

    Update dodawanie_sekwencyjne.c

--- Liczby Pierwsze/dodawanie_sekwencyjne.c
+++ Liczby Pierwsze/dodawanie_sekwencyjne.c
@@ -5,7 +5,6 @@
 
 int main(){
     //Tworzenie potrzebnych zmiennych
-    int min = 2;
     int max = 100000000;
     int max_sqrt = sqrt(max);
     int primes_amount = 0;
@@ -13,16 +12,22 @@ int main(){
     int* sieve_eratosthenes = malloc(max * sizeof(int));
 
     //Ustawienie sita na wartosci 1
-    for(int i = 0; i < max + 1; i++){
+    for(int i = 0; i < max; i++){
         sieve_eratosthenes[i] = 1;
     }
 
     //Sprawdzenie liczb czy sa pierwsze metoda sita Eratostenesa
     for(int i = 2; i <= max_sqrt; i++){
         if(sieve_eratosthenes[i]){
-            for(int j = 2*i; j < max; j += i){
+            for(int j = 2*i; j <= max; j += i){
                 sieve_eratosthenes[j] = 0;
             }
+        }
+    }
+
+    //Podliczanie ilosci liczb pierwszych
+    for(int i = 2; i < max; i++){
+        if(sieve_eratosthenes[i]){
             primes_amount++;
         }
     }
