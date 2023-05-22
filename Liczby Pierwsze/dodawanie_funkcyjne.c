c8db0853a432be29a48b1932b2614947d7d010c6
Filip Filipiak <115704204+Turitenz@users.noreply.github.com>
2023-05-23 01:23:10 +0200

    Update dodawanie_funkcyjne.c

--- Liczby Pierwsze/dodawanie_funkcyjne.c
+++ Liczby Pierwsze/dodawanie_funkcyjne.c
@@ -3,37 +3,57 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
-    int max = 100000000;
-    int max_sqrt = sqrt(max);
-    int primes_amount = 0;
+void calculate_sieve(int* sieve_eratosthenes, int start, int end) {
     int i, j;
 
-    int* sieve_eratosthenes = malloc(max * sizeof(int));
-
-    //Ustawienie sita na wartosci 1
-    for(i = 0; i < max + 1; i++){
-        sieve_eratosthenes[i] = 1;
-    }
-
-    #pragma omp parallel for shared(sieve_eratosthenes, primes_amount) private(i, j)
-    for(i = 2; i <= max_sqrt; i++){
-        if(sieve_eratosthenes[i]){
-            for(j = 2*i; j < max; j += i){
+#pragma omp parallel for shared(sieve_eratosthenes) private(i, j)
+    for (i = start; i <= end; i++) {
+        if (sieve_eratosthenes[i]) {
+            for (j = 2 * i; j <= end; j += i) {
                 sieve_eratosthenes[j] = 0;
             }
         }
     }
+}
 
-    #pragma omp for schedule(guided)
-    for(i = 2; i < max; i ++){
-        if(sieve_eratosthenes[i]){
-            #pragma omp atomic
+int main() {
+    //Tworzenie potzebnych zmiennych
+    int max = 100000000;
+    int max_sqrt = sqrt(max);
+    int primes_amount = 0;
+    int thread_number = 8;
+
+    int* sieve_eratosthenes = new int[max];
+
+    //Ustawienie sita na wartosci 1
+    for (int i = 0; i < max; i++) {
+        sieve_eratosthenes[i] = 1;
+    }
+
+    //Sprawdzenie liczb czy sa pierwsze metoda sita Eratostenesa
+#pragma omp parallel for schedule(static)
+    for (int i = 0; i < thread_number; i++) {
+        int start = i * (max / thread_number) + 1;
+        int end = (i + 1) * (max / thread_number);
+        int x, y;
+#pragma omp parallel for shared(sieve_eratosthenes) private(x, y)
+        for (x = start; x <= end; x++) {
+            if (sieve_eratosthenes[x]) {
+                for (y = 2 * x; y <= end; y += x) {
+                    sieve_eratosthenes[y] = 0;
+                }
+            }
+        }
+    }
+
+#pragma omp parallel for reduction(+:primes_amount)
+    for (int i = 2; i < max; i++) {
+        if (sieve_eratosthenes[i]) {
             primes_amount++;
         }
     }
 
-    printf("[Dodawanie Funkcyjne] Ilosc liczb pierwszych: %d", primes_amount);
+    printf("[Dodawanie Domenowe] Ilosc liczb pierwszych: %d", primes_amount);
 
     return 0;
 }
