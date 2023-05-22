a9bc381c7d1e3c17b8dc96dfaa546b7f82c3cb44
Wojciech Galewski <81717259+wojgal@users.noreply.github.com>
2023-05-23 01:47:49 +0200

    Update dodawanie_domenowe.c

--- Liczby Pierwsze/dodawanie_domenowe.c
+++ Liczby Pierwsze/dodawanie_domenowe.c
@@ -10,7 +10,7 @@ int main() {
     int primes_amount = 0;
     int thread_number = 8;
 
-    int* sieve_eratosthenes = new int[max];
+    int* sieve_eratosthenes = (int*)malloc(max * sizeof(int));
 
     //Ustawienie sita na wartosci 1
     for (int i = 0; i < max; i++) {
@@ -18,23 +18,28 @@ int main() {
     }
 
     //Sprawdzenie liczb czy sa pierwsze metoda sita Eratostenesa
-#pragma omp parallel for schedule(static)
+//#pragma omp parallel for schedule(static)
     for (int i = 0; i < thread_number; i++) {
         int start = i * (max / thread_number) + 1;
+
+        if(start < 2){
+            start = 2;
+        }
+        
         int end = (i + 1) * (max / thread_number);
         int x, y;
 
-#pragma omp parallel for private(x, y)
+//#pragma omp parallel for private(x, y)
         for (x = start; x <= end; x++) {
             if (sieve_eratosthenes[x]) {
-                for (y = 2 * x; y <= end; y += x) {
+                for (y = 2 * x; y <= max; y += x) {
                     sieve_eratosthenes[y] = 0;
                 }
             }
         }
     }
 
-#pragma omp parallel for reduction(+:primes_amount)
+//#pragma omp parallel for reduction(+:primes_amount)
     for (int i = 2; i < max; i++) {
         if (sieve_eratosthenes[i]) {
             primes_amount++;
