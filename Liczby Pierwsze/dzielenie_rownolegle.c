950510ce9aaa7b9e240547ecd33ae1a9cb16e599
Filip Filipiak <115704204+Turitenz@users.noreply.github.com>
2023-05-23 20:06:45 +0200

    Update dzielenie_rownolegle.c - final

--- Liczby Pierwsze/dzielenie_rownolegle.c
+++ Liczby Pierwsze/dzielenie_rownolegle.c
@@ -5,16 +5,11 @@
 
 //Funkcja sprawdzajaca dzieleniem, czy liczba jest pierwsza
 int check_prime(int number){
-    if(number < 2){
-        return 0;
-    }
-
     for(int i = 2; i <= sqrt(number); i++){
         if(number % i == 0){
             return 0;
         }
     }
-
     return 1;
 }
 
@@ -22,17 +17,15 @@ int main(){
     //Tworzenie potrzebnych zmiennych
     int min = 2;
     int max = 100000000;
-    int max_sqrt = sqrt(max);
     int primes_amount = 0;
 
     //Sprawdzanie liczb czy sa pierwsze 
-    #pragma omp parallel shared(primes_amount)
-    #pragma omp parallel for schedule(guided)
-    for(int i = 0; i <= max; i++){
+    #pragma omp parallel for schedule(guided) reduction(+:primes_amount)
+    for(int i = min; i <= max; i++){
         primes_amount += check_prime(i);
     }
 
-    printf("[Dzielenie Sekwencyjne] Ilosc liczb pierwszych: %d", primes_amount);
+    printf("[Dzielenie Rownolegle] Ilosc liczb pierwszych: %d", primes_amount);
 
     return 0;
 }
