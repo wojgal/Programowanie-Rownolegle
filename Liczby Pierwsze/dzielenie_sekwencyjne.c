94d5346fffdcca8cd52a9805bb23893ff01ba5d8
Filip Filipiak <115704204+Turitenz@users.noreply.github.com>
2023-05-24 19:20:47 +0200

    Update dzielenie_sekwencyjne.c

--- Liczby Pierwsze/dzielenie_sekwencyjne.c
+++ Liczby Pierwsze/dzielenie_sekwencyjne.c
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 //Funkcja sprawdzajaca dzieleniem, czy liczba jest pierwsza
 int check_prime(int number){
@@ -18,18 +19,48 @@ int check_prime(int number){
     return 1;
 }
 
-int main(){
-    //Tworzenie potrzebnych zmiennych
-    int max = 100000000;
-    int max_sqrt = sqrt(max);
+void print(bool* sieve_eratosthenes, int min, int max){
     int primes_amount = 0;
+    int counter = 0;
 
-    //Sprawdzanie liczb czy sa pierwsze 
-    for(int i = 0; i <= max; i++){
-        primes_amount += check_prime(i);
+    printf("Liczby pierwsze dla przedzialu %d - %d:\n\n", min, max);
+    for(int i = 0; i <= max - min + 1; i++){
+        if(sieve_eratosthenes[i]){
+            printf("%d ", i + min);
+            primes_amount++;
+            counter++;
+        }
+        if(counter == 10){
+            counter = 0;
+            printf("\n");
+        }
     }
 
-    printf("[Dzielenie Sekwencyjne] Ilosc liczb pierwszych: %d", primes_amount);
+    printf("\n\nIlosc liczb pierwszych w przedziale %d - %d wynosi: %d\n", min, max, primes_amount);
+}
+
+int main(){
+    //Tworzenie potrzebnych zmiennych
+    int min = 50;
+    int max = 100;
+    int max_sqrt = sqrt(max);
+    int primes_amount = 0;
+    bool prime;
+    bool* sieve_eratosthenes = (bool *)calloc(max-min+1, sizeof(bool));
+
+    //Zmienna odpowiadajca za wypisywanie 
+    bool print_result = false;
+
+    //Sprawdzanie liczb czy sa pierwsze 
+    for(int i = min; i <= max; i++){
+        prime = check_prime(i);
+        primes_amount += prime;
+        sieve_eratosthenes[i - min] = prime;
+    }
+
+    if(print_result){
+        print(sieve_eratosthenes, min, max);
+    }
 
     return 0;
 }
