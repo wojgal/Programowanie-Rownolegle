96ecebbb8c358fc0156610422eca1a7022c29409
Wojciech Galewski <81717259+wojgal@users.noreply.github.com>
2023-05-29 15:46:41 +0200

    Update test.c

--- Liczby Pierwsze/test.c
+++ Liczby Pierwsze/test.c
@@ -1,50 +1,104 @@
 #include <stdio.h>
+#include <omp.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
-#include <omp.h>
+
+void print(bool* sieve_eratosthenes, int min, int max) {
+    // Jak wcześniej
+    // ...
+}
+
+void get_start_primes(bool* start_primes, int min) {
+    // Jak wcześniej
+    // ...
+}
 
 int main() {
-    __uint64_t max = 2000000000;
+    int min = 2;
+    int max = 1000000000;
     int max_sqrt = sqrt(max);
-    __uint64_t primes_amount = 0;
     int thread_number = 8;
+    bool* start_primes = (bool*)calloc(min - 2, sizeof(bool));
+    bool* sieve_eratosthenes = (bool*)calloc(max - min + 1, sizeof(bool));
+    bool print_result = false;
 
-    bool* sieve_eratosthenes = (bool *)calloc(max, sizeof(bool));
-
-    // Ustawienie sita na wartości 1
-    #pragma omp parallel for schedule(guided)
-    for (__uint64_t i = 0; i < max; i++) {
-        sieve_eratosthenes[i] = true;
+    if (min > 2) {
+        get_start_primes(start_primes, min);
     }
 
-    // Sprawdzenie liczb czy są pierwsze metodą sita Eratostenesa
-    #pragma omp parallel num_threads(thread_number)
-    {
-        int tid = omp_get_thread_num();
-        __uint64_t start = tid * (max / thread_number) + 1;
-        if (start < 2) {
-            start = 2;
-        }
-        __uint64_t end = (tid + 1) * (max / thread_number);
+    // Usunięcie linii: omp_set_num_threads(8);
+    // Liczba wątków zostanie automatycznie określona przez OpenMP
 
-        for (__uint64_t x = start; x <= end; x++) {
-            if (sieve_eratosthenes[x]) {
-                for (__uint64_t y = 2 * x; y <= max; y += x) {
-                    sieve_eratosthenes[y] = false;
+    #pragma omp parallel
+    {
+        // Każdy wątek ma swoją kopię zmiennych
+        bool* thread_start_primes = (bool*)calloc(min - 2, sizeof(bool));
+        bool* thread_sieve_eratosthenes = (bool*)calloc(max - min + 1, sizeof(bool));
+
+        // Każdy wątek inicjalizuje lokalne kopie start_primes i sieve_eratosthenes
+        for (int i = 2; i < min; i++) {
+            thread_start_primes[i - 2] = 1;
+        }
+
+        for (int i = 2; i <= sqrt(min); i++) {
+            if (thread_start_primes[i - 2]) {
+                for (int j = 2 * i; j < min; j += i) {
+                    thread_start_primes[j - 2] = 0;
                 }
             }
         }
-    }
 
-    #pragma omp parallel for schedule(guided) reduction(+:primes_amount)
-    for (__uint64_t i = 2; i < max; i++) {
-        if (sieve_eratosthenes[i]) {
-            primes_amount++;
+        // Każdy wątek wykonuje obliczenia na swoim zakresie
+        #pragma omp for schedule(dynamic)
+        for (int x = 0; x < thread_number; x++) {
+            int start = x * (max / thread_number) + 1;
+            if (start < 2) {
+                start = 2;
+            }
+            int end = (x + 1) * (max / thread_number);
+            if (x == thread_number - 1) {
+                end = max;
+            }
+
+            for (int i = start; i <= end; i++) {
+                if (i < min) {
+                    if (thread_start_primes[i - 2]) {
+                        int remainder = min % i;
+                        for (int j = min - remainder; j <= max; j += i) {
+                            thread_sieve_eratosthenes[j - min] = 0;
+                        }
+                    }
+                }
+                else {
+                    if (thread_sieve_eratosthenes[i - min]) {
+                        for (int j = 2 * i; j <= max; j += i) {
+                            thread_sieve_eratosthenes[j - min] = 0;
+                        }
+                    }
+                }
+            }
         }
+
+        // Połączenie wyników z każdego wątku w tablicach współdzielonych
+        #pragma omp critical
+        {
+            for (int i = 0; i <= max - min; i++) {
+                sieve_eratosthenes[i] |= thread_sieve_eratosthenes[i];
+            }
+        }
+
+        // Zwolnienie pamięci zaalokowanej dla lokalnych tablic
+        free(thread_start_primes);
+        free(thread_sieve_eratosthenes);
     }
 
-    printf("[Dodawanie Domenowe] Ilość liczb pierwszych: %ld\n", primes_amount);
+    if (print_result) {
+        print(sieve_eratosthenes, min, max);
+    }
+
+    free(start_primes);
     free(sieve_eratosthenes);
+
     return 0;
 }
