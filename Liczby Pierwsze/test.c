a615273c719a18db680257c4780fe5bf75f151ab
Wojciech Galewski <81717259+wojgal@users.noreply.github.com>
2023-05-29 16:22:57 +0200

    Update test.c

--- Liczby Pierwsze/test.c
+++ Liczby Pierwsze/test.c
@@ -5,13 +5,37 @@
 #include <stdbool.h>
 
 void print(bool* sieve_eratosthenes, int min, int max) {
-    // Jak wcześniej
-    // ...
+    int primes_amount = 0;
+    int counter = 0;
+
+    printf("Liczby pierwsze dla przedzialu %d - %d:\n\n", min, max);
+    for (int i = 0; i <= max - min; i++) {
+        if (sieve_eratosthenes[i]) {
+            printf("%d ", i + min);
+            primes_amount++;
+            counter++;
+        }
+        if (counter == 10) {
+            counter = 0;
+            printf("\n");
+        }
+    }
+
+    printf("\n\nIlosc liczb pierwszych w przedziale %d - %d wynosi: %d\n", min, max, primes_amount);
 }
 
 void get_start_primes(bool* start_primes, int min) {
-    // Jak wcześniej
-    // ...
+    for (int i = 0; i <= min - 2; i++) {
+        start_primes[i] = true;
+    }
+
+    for (int i = 2; i <= sqrt(min); i++) {
+        if (start_primes[i - 2]) {
+            for (int j = 2 * i; j <= min; j += i) {
+                start_primes[j - 2] = false;
+            }
+        }
+    }
 }
 
 int main() {
@@ -21,35 +45,16 @@ int main() {
     int thread_number = 8;
     bool* start_primes = (bool*)calloc(min - 2, sizeof(bool));
     bool* sieve_eratosthenes = (bool*)calloc(max - min + 1, sizeof(bool));
-    bool print_result = false;
+    bool print_result = true; // Zmieniono na true w celu wypisania wyników
 
     if (min > 2) {
         get_start_primes(start_primes, min);
     }
 
-    // Usunięcie linii: omp_set_num_threads(8);
-    // Liczba wątków zostanie automatycznie określona przez OpenMP
-
     #pragma omp parallel
     {
-        // Każdy wątek ma swoją kopię zmiennych
-        bool* thread_start_primes = (bool*)calloc(min - 2, sizeof(bool));
         bool* thread_sieve_eratosthenes = (bool*)calloc(max - min + 1, sizeof(bool));
 
-        // Każdy wątek inicjalizuje lokalne kopie start_primes i sieve_eratosthenes
-        for (int i = 2; i < min; i++) {
-            thread_start_primes[i - 2] = 1;
-        }
-
-        for (int i = 2; i <= sqrt(min); i++) {
-            if (thread_start_primes[i - 2]) {
-                for (int j = 2 * i; j < min; j += i) {
-                    thread_start_primes[j - 2] = 0;
-                }
-            }
-        }
-
-        // Każdy wątek wykonuje obliczenia na swoim zakresie
         #pragma omp for schedule(dynamic)
         for (int x = 0; x < thread_number; x++) {
             int start = x * (max / thread_number) + 1;
@@ -63,24 +68,23 @@ int main() {
 
             for (int i = start; i <= end; i++) {
                 if (i < min) {
-                    if (thread_start_primes[i - 2]) {
+                    if (start_primes[i - 2]) {
                         int remainder = min % i;
                         for (int j = min - remainder; j <= max; j += i) {
-                            thread_sieve_eratosthenes[j - min] = 0;
+                            thread_sieve_eratosthenes[j - min] = false;
                         }
                     }
                 }
                 else {
                     if (thread_sieve_eratosthenes[i - min]) {
                         for (int j = 2 * i; j <= max; j += i) {
-                            thread_sieve_eratosthenes[j - min] = 0;
+                            thread_sieve_eratosthenes[j - min] = false;
                         }
                     }
                 }
             }
         }
 
-        // Połączenie wyników z każdego wątku w tablicach współdzielonych
         #pragma omp critical
         {
             for (int i = 0; i <= max - min; i++) {
@@ -88,8 +92,6 @@ int main() {
             }
         }
 
-        // Zwolnienie pamięci zaalokowanej dla lokalnych tablic
-        free(thread_start_primes);
         free(thread_sieve_eratosthenes);
     }
 
