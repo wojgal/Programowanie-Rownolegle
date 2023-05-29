d7781acfddb865fea8c6ed1a4b60c330ae8b6193
Wojciech Galewski <81717259+wojgal@users.noreply.github.com>
2023-05-29 17:05:46 +0200

    Update test.c

--- Liczby Pierwsze/test.c
+++ Liczby Pierwsze/test.c
@@ -4,12 +4,18 @@
 #include <omp.h>
 
 #define N 100000001
-#define CHUNK_SIZE 10000
 
 int main() {
     bool *isPrime = (bool *)malloc(N * sizeof(bool));
     if (isPrime == NULL) {
         printf("Błąd alokacji pamięci.\n");
+        return 1;
+    }
+
+    bool *tmp = (bool *)malloc(N * sizeof(bool));
+    if (tmp == NULL) {
+        printf("Błąd alokacji pamięci.\n");
+        free(isPrime);
         return 1;
     }
 
@@ -21,21 +27,31 @@ int main() {
     }
 
     // Zrównoleglenie obliczeń przy użyciu OpenMP
-    #pragma omp parallel shared(isPrime) private(i, j)
+    #pragma omp parallel shared(isPrime, tmp) private(i, j)
     {
         int numThreads = omp_get_num_threads();
         int threadId = omp_get_thread_num();
-        int numChunks = N / CHUNK_SIZE;
-        int chunkStart = threadId * numChunks;
-        int chunkEnd = chunkStart + numChunks;
+        int chunkSize = (N - 2) / numThreads;
+        int start = 2 + threadId * chunkSize;
+        int end = start + chunkSize;
 
-        // Obszar obliczeń dla każdego wątku
-        for (i = 2 + chunkStart; i < N && i < 2 + chunkEnd; i++) {
+        // Obliczenia na tablicy pomocniczej
+        for (i = start; i < end; i++) {
+            tmp[i] = true;
+        }
+
+        for (i = 2; i * i < N; i++) {
             if (isPrime[i]) {
                 for (j = i * i; j < N; j += i) {
-                    isPrime[j] = false;
+                    tmp[j] = false;
                 }
             }
+        }
+
+        // Kopiowanie wyników do tablicy isPrime
+        #pragma omp barrier
+        for (i = start; i < end; i++) {
+            isPrime[i] = tmp[i];
         }
     }
 
@@ -46,7 +62,4 @@ int main() {
         }
     }
 
-    free(isPrime);  // Zwolnienie pamięci
-
-    return 0;
-}
+    free(isPrime);  // Zwolnienie
