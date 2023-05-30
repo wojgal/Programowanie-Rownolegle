7f2605b341f61cb1eb1af964b1c8fe3e7d042efd
Filip Filipiak <115704204+Turitenz@users.noreply.github.com>
2023-05-30 17:39:37 +0200

    Update dodawanie_domenowe.c

--- Liczby Pierwsze/dodawanie_domenowe.c
+++ Liczby Pierwsze/dodawanie_domenowe.c
@@ -18,7 +18,7 @@ void print(bool* sieve_eratosthenes, int min, int max) {
         }
         if (counter == 10) {
             counter = 0;
-            printf("\n");
+           printf("\n");
         }
     }
 
@@ -26,11 +26,12 @@ void print(bool* sieve_eratosthenes, int min, int max) {
 }
 
 void get_start_primes(bool* start_primes, int min) {
+#pragma omp parallel for schedule(guided)
     for (int i = 2; i < min; i++) {
         start_primes[i - 2] = 1;
     }
-
-    for (int i = 2; i <= sqrt(min); i++) {
+#pragma omp parallel for schedule(dynamic)
+    for (int i = 2; i <= (int)sqrt(min); i++) {
         if (start_primes[i - 2]) {
             for (int j = 2 * i; j < min; j += i) {
                 start_primes[j - 2] = 0;
@@ -40,69 +41,94 @@ void get_start_primes(bool* start_primes, int min) {
 }
 
 int main() {
-    //Tworzenie potzebnych zmiennych
-    int min = 2;
-    int max = 1000000000;
-    int max_sqrt = sqrt(max);
-    int thread_number = 8;
-    bool* start_primes = (bool*)calloc(min - 2, sizeof(bool));
+        int min = 2;
+        int max = 100000000;
+        int max_sqrt = sqrt(max);
 
-    bool* sieve_eratosthenes = (bool*)calloc(max - min + 1, sizeof(bool));
+        bool* sieve_eratosthenes = (bool*)calloc((max), sizeof(bool));
+        bool* start_primes = (bool*)calloc(min - 2, sizeof(bool));
+        omp_set_num_threads(8);
+        //Zmienna odpowiadajca za wypisywanie 
+        bool print_result = false;
+        int thread_number = 8;
 
-    //Zmienna odpowiadajca za wypisywanie 
-    bool print_result = false;
-
-    if (min > 2) {
-        get_start_primes(start_primes, min);
-    }
-
-    //Ustawienie sita na wartosci 1
-    for (int i = min; i <= max; i++) {
-        sieve_eratosthenes[i - min] = 1;
-    }
-    omp_set_num_threads(8);
-    //Sprawdzenie liczb czy sa pierwsze metoda sita Eratostenesa
-#pragma omp parallel for schedule(dynamic) shared(sieve_eratosthenes)
-    for (int x = 0; x < thread_number; x++) {
-        int start = x * (max / thread_number) + 1;
-
-        if (start < 2) {
-            start = 2;
-        }
-        int end = (x + 1) * (max / thread_number);
-        if (x == thread_number - 1) {
-            end = max;
+        if (min > 2) {
+            //start_primes = (bool *)calloc(min - 2, sizeof(bool));
+            get_start_primes(start_primes, min);
         }
 
+        //Ustawienie sita na wartosci 1
+#pragma omp parallel for schedule(guided) shared(sieve_eratosthenes)
+        for (int i = min; i <= max; i++) {
+            sieve_eratosthenes[i - min] = 1;
+        }
 
-        for (int i = start; i <= end; i++) {
+        for (int i = 2; i <= max_sqrt; i++) {
             //Liczby przed zakresem
             if (i < min) {
                 if (start_primes[i - 2]) {
-                    int remainder = min % i;
 
-                    for (int j = min - remainder; j <= max; j += i) {
-                        sieve_eratosthenes[j - min] = 0;
+#pragma omp parallel
+                    {
+                        int thread_id = omp_get_thread_num();
+                        int start = thread_id * ((max - min - i + 1 )/ thread_number) + 1;
+                        int end = (thread_id + 1) * ((max - min -i + 1) / thread_number);
+
+                        if (thread_id == 0) 
+                            start = start;
+
+                        if (thread_id == thread_number - 1)
+                            end = max;
+
+                        int remainder = start % i;
+
+                        for (int j = start - remainder; j <= end; j += i) {
+                            if (start - remainder == i) {
+                                continue;
+                            }
+                            if (j >= min)
+                                sieve_eratosthenes[j - min] = 0;
+                        }
                     }
                 }
             }
             //Liczby po zakresie
             else {
                 if (sieve_eratosthenes[i - min]) {
-                    for (int j = 2 * i; j <= max; j += i) {
-                        sieve_eratosthenes[j - min] = 0;
+                    #pragma omp parallel
+                    {
+
+                        int thread_id = omp_get_thread_num();
+                        int start = thread_id * ((max - i + 1) / thread_number) + 1;
+                        int end = (thread_id + 1) * ((max - i + 1) / thread_number);
+
+                        if (thread_id == 0) {
+                            start = 2*i;
+                        }
+
+                        if (thread_id == thread_number - 1) {
+                            end = max;
+                        }
+
+                        int remainder = start % i;
+
+                        for (int j = start - remainder; j <= end; j += i) {
+                            if (start - remainder == i) {
+                                continue;
+                            }
+                            sieve_eratosthenes[j - min] = 0;
+                        }
                     }
                 }
             }
         }
-    }
 
-    if (print_result) {
-        print(sieve_eratosthenes, min, max);
-    }
+        if (print_result) {
+            print(sieve_eratosthenes, min, max);
+        }
 
-    free(start_primes);
-    free(sieve_eratosthenes);
+        //free(sieve_eratosthenes);
+        //free(start_primes);
 
     return 0;
 }
